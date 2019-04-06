ebd26c9f5dedcfee47861f370d774792dffef812
Yukiyo Torai <y.torai@nit.local>
2019-04-06 16:05:22 +0100

    Change port for heroku

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post("/", function(req, res) {
 app.post("/failure", function(req, res) {
   res.redirect('/');
 });
-app.listen((3000 || process.evn.PORT), function() {
+app.listen((process.evn.PORT), function() {
   console.log("The server is running on port 3000");
 });
 
